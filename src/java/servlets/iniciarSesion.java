b5d412da6d93041092f6f4928ce4c70fb5ceb607
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2021-01-25 13:16:49 -0600

    servlets para inicio de sesion y jdbc

---
+++ src/java/servlets/iniciarSesion.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author calebbolanos
+ */
+public class iniciarSesion extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>\n"
+                    + "    <head>\n"
+                    + "        <title>Generador de examenes</title>\n"
+                    + "        <meta charset=\"UTF-8\">\n"
+                    + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                    + "        <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">\n"
+                    + "        <link rel=\"stylesheet\" href=\"https://code.getmdl.io/1.3.0/material.blue-indigo.min.css\" />\n"
+                    + "        <script defer src=\"https://code.getmdl.io/1.3.0/material.min.js\"></script>\n"
+                    + "        <style>\n"
+                    + "            .tarjeta {\n"
+                    + "                width: 320px;\n"
+                    + "                height: auto;\n"
+                    + "                padding: 20px;\n"
+                    + "                margin-top: 50px;\n"
+                    + "                box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);\n"
+                    + "                transition: all 0.3s cubic-bezier(.25,.8,.25,1);\n"
+                    + "            }\n"
+                    + "\n"
+                    + "            .tarjeta:hover {\n"
+                    + "                box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);\n"
+                    + "            }\n"
+                    + "\n"
+                    + "            .layout {\n"
+                    + "                display: flex;\n"
+                    + "                justify-content: center;\n"
+                    + "                align-items: center;\n"
+                    + "            }\n"
+                    + "        </style>\n"
+                    + "    </head>\n"
+                    + "    <body>\n"
+                    + "        <div class=\"layout\">\n"
+                    + "            <div class=\"tarjeta\">\n"
+                    + "                <h2 class=\"mdl-card__title-text\">Iniciar Sesion</h2>\n"
+                    + "                <form method=\"POST\" action=\"#\">\n"
+                    + "                    <div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\">\n"
+                    + "                        <input class=\"mdl-textfield__input\" type=\"text\" id=\"usuario\" name=\"usuario\" required>\n"
+                    + "                        <label class=\"mdl-textfield__label\" for=\"usuario\">Usuario</label>\n"
+                    + "                    </div>\n"
+                    + "                    <div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\">\n"
+                    + "                        <input class=\"mdl-textfield__input\" type=\"password\" id=\"contrasena\" name=\"contrasena\" required>\n"
+                    + "                        <label class=\"mdl-textfield__label\" for=\"contrasena\">Contrasena</label>\n"
+                    + "                    </div>\n"
+                    + "                    <input type=\"submit\" value=\"Iniciar Sesion\" class=\"mdl-button mdl-js-button mdl-button--raised mdl-button--accent\" />\n"
+                    + "                </form>\n"
+                    + "            </div>\n"
+                    + "        </div>\n"
+                    + "    </body>\n"
+                    + "</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
